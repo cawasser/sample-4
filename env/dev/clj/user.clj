9594420f412fe9568ed72f610838e77566988da1
Chris Wasser <cawasser@mac.com>
2019-10-01 17:57:02 -0400

    fixing problems with the way the Luminus template works. NOTE: adding dev-config.edn to a repository is actually pretty dangerous given what could be stored in it, but for this simple example it makes the samples tasks much easier.

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -4,7 +4,7 @@
     [clojure.spec.alpha :as s]
     [expound.alpha :as expound]
     [mount.core :as mount]
-    [sample-4.figwheel :refer [start-fw stop-fw cljs]]
+    ;[sample-4.figwheel :refer [start-fw stop-fw cljs]]
     [sample-4.core :refer [start-app]]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
