4cd10584b64b5aa4c58cd09ad30429164d78dde3
George Pierce <gpierce3@hotmail.com>
2019-10-02 11:05:45 -0400

    changed code to use my name in greeting

--- src/cljs/sample_4/core.cljs
+++ src/cljs/sample_4/core.cljs
@@ -48,7 +48,7 @@
 (defn home-page []
   [:section.section>div.container>div.content
    [welcome-hero "ClojureLand" "Here we go!" "is-info"]
-   [welcome-hero "Chris Wasser" "Welcome to ClojureLand, everyone!" "is-warning"]])
+   [welcome-hero "George Pierce" "Welcome to ClojureLand, everyone!" "is-warning"]])
 
 (def pages
   {:home #'home-page
